a240d2e48a796359b94b8c7ec1823280405fc453
Tudor Haba <tudor.haba@softvision.ro>
2019-05-02 14:57:24 +0300

    [#29] Preparing for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,12 +93,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
+    address: "smtp.gmail.com",
     port: 587,
     domain: "mail.google.com",
     user_name: "TODO",
     password: "TODO",
-    authentication: “plain”,
+    authentication: "plain",
     enable_starttls_auto: true
   }
 end
